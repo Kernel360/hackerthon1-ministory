5fe8eeb4160bd7283e5ad7dc2a236212a93e4576
anso33 <hyeon131812@gmail.com>
2023-10-16 15:55:14 +0900

    chore: Set first project

---
+++ ministory/src/main/java/com/example/ministory/MinistoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.ministory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinistoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinistoryApplication.class, args);
+	}
+
+}
---
+++ ministory/src/test/java/com/example/ministory/MinistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ministory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinistoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
