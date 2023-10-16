3a942ae14c2af6993ef69c18e902e269b000f3ef
isevou__ <kbry12@naver.com>
2023-10-17 03:24:11 +0900

    refactor : add getSignUp and postSignUp method in UserApiController

--- ministory/src/main/java/com/example/ministory/controller/UserApiController.java
+++ ministory/src/main/java/com/example/ministory/controller/UserApiController.java
@@ -1,16 +1,20 @@
 package com.example.ministory.controller;
 
+import com.example.ministory.dto.UserEntityDto;
 import com.example.ministory.entity.UserEntity;
 import com.example.ministory.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RequiredArgsConstructor
-@RestController
+@Controller
 @RequestMapping("/api/user")
 public class UserApiController {
 
@@ -19,5 +23,18 @@ public class UserApiController {
     @GetMapping("/find-all")
     public List<UserEntity> findAll() {
         return userRepository.findAll();
+    }
+
+    @GetMapping("/signup")
+    public String getSignUp(UserEntityDto userEntityDto) {
+        return "userCreateForm";
+    }
+
+    @PostMapping("/signup")
+    public String postSignUp(@Valid UserEntityDto userEntityDto, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "userCreateForm";
+        }
+        return "redirect:/";
     }
 }
