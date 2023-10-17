41f1ffb92d0e944ec64a145dfbe6bc36b70ac943
daniel <kbry12@naver.com>
2023-10-17 12:35:02 +0900

    Test : Add UserService, UserRepository to test userCreateForm

---
+++ ministory/src/main/java/com/example/ministory/controller/UserController.java
@@ -0,0 +1,46 @@
+package com.example.ministory.controller;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.ministory.dto.UserDto;
+import com.example.ministory.entity.User;
+import com.example.ministory.service.UserService;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Controller
+@RequestMapping("/user")
+public class UserController {
+    private UserService userService;
+
+    @GetMapping("/signup")
+    public String getSignUp(UserDto userDto) {
+        return "userCreateForm";
+    }
+
+    @PostMapping("/signup")
+    public String postSignUp(@Valid UserDto userDto, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "userCreateForm";
+        }
+        userService.saveUser(userDto);
+        return "redirect:/";
+    }
+
+    @GetMapping("/list")
+    public String getUserList(Model model) {
+        List<User> users = userService.findUsers();
+        model.addAttribute("users", users);
+        return "userlist";
+    }
+}
--- ministory/src/main/java/com/example/ministory/dto/UserDto.java
+++ ministory/src/main/java/com/example/ministory/dto/UserDto.java
@@ -1,27 +1,32 @@
 package com.example.ministory.dto;
 
-import com.example.ministory.entity.UserEntity;
+import java.sql.Timestamp;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+import com.example.ministory.entity.User;
+
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-import javax.validation.constraints.*;
-import java.util.Date;
-
 @Getter
 @Setter
 @ToString
 @NoArgsConstructor
-public class UserEntityDto {
+public class UserDto {
     private Long userId;
 
     @Size(min = 4, message = "닉네임은 4글자 이상 이어야 합니다")
     @NotBlank(message = "닉네임은 필수항목입니다.")
     private String nickname;
 
-    @Past
-    private Date birthday;
+    private String birthday;
 
     private String imagePath;
 
@@ -33,19 +38,30 @@ public class UserEntityDto {
     @NotEmpty(message = "비밀번호는 필수항목입니다.")
     private String password;
 
-    private String passwordConfirm;
-
-    private boolean  isSocial;
+    private boolean isSocial;
 
     @Size(min = 4, message = "블로그 주소는 4글자 이상 이어야 합니다")
     @NotBlank(message = "블로그 주소는 필수항목입니다.")
     private String address;
 
-    public UserEntity toEntity() {
-        UserEntity userEntity = UserEntity.builder()
+    @Builder
+    public UserDto(Long userId, String nickname, String birthday, String imagePath, String email, String password,
+        boolean isSocial, String address) {
+        this.userId = userId;
+        this.nickname = nickname;
+        this.birthday = birthday;
+        this.imagePath = imagePath;
+        this.email = email;
+        this.password = password;
+        this.isSocial = isSocial;
+        this.address = address;
+    }
+
+    public User toEntity() {
+        User user = User.builder()
                 .userId(userId)
                 .nickname(nickname)
-                .birthday(birthday)
+               .birthday(Timestamp.valueOf(birthday + " 00:00:00"))
                 .imagePath(imagePath)
                 .email(email)
                 .password(password)
@@ -53,6 +69,6 @@ public class UserEntityDto {
                 .address(address)
                 .build();
 
-        return userEntity;
+        return user;
     }
 }
