f6cf80811cae52d3de9a1a87beb07cb1371b495a
daniel <kbry12@naver.com>
2023-10-17 15:49:47 +0900

    feat : add methods for saving comment on comment and comment on post

---
+++ ministory/src/main/java/com/example/ministory/service/CommentService.java
@@ -0,0 +1,40 @@
+package com.example.ministory.service;
+
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+
+import com.example.ministory.dto.CommentDto;
+import com.example.ministory.entity.Comment;
+import com.example.ministory.entity.Post;
+import com.example.ministory.entity.User;
+import com.example.ministory.repository.CommentRepository;
+import com.example.ministory.repository.PostRepository;
+import com.example.ministory.repository.UserRepository;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@Transactional
+@AllArgsConstructor
+public class CommentService {
+	public CommentRepository commentRepository;
+	public UserRepository userRepository;
+	public PostRepository postRepository;
+
+	public void saveCommentOnPost(CommentDto commentDto, Long userId, Long postId) {
+		User user = userRepository.findById(userId).orElseThrow(()-> new RuntimeException());
+		Post post = postRepository.findById(postId).orElseThrow(() -> new RuntimeException());
+		Comment comment = commentDto.toEntity(user, post);
+
+		commentRepository.save(comment);
+	}
+
+	public void saveCommentOnComment(CommentDto commentDto, Long userId, Long postId, Long parentId) {
+		User user = userRepository.findById(userId).orElseThrow(()-> new RuntimeException());
+		Post post = postRepository.findById(postId).orElseThrow(() -> new RuntimeException());
+		Comment comment = commentDto.toEntity(user, post, parentId);
+
+		commentRepository.save(comment);
+	}
+}
