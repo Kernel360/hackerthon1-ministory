23e9ea1b998fda1f6a85203d6f6facba756b0205
anso33 <hyeon131812@gmail.com>
2023-10-17 09:50:35 +0900

    fix: Add LazyLoading on likes and scrap of post field

--- ministory/src/main/java/com/example/ministory/entity/Likes.java
+++ ministory/src/main/java/com/example/ministory/entity/Likes.java
@@ -1,6 +1,7 @@
 package com.example.ministory.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -22,7 +23,7 @@ public class Likes {
 	@JoinColumn(name = "user_id")
 	private User user;
 
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "post_id")
 	private Post post;
 }
