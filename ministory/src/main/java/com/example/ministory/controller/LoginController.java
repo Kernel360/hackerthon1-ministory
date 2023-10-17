df18b8a183733aba3d3d09b2af4041b0443c3410
StoneHee99 <kiit0901@gmail.com>
2023-10-17 10:33:38 +0900

    chore: add thymeleaf dependency and controller template that returns thymeleaf model (LoginController)

---
+++ ministory/src/main/java/com/example/ministory/controller/LoginController.java
@@ -0,0 +1,13 @@
+package com.example.ministory.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+    @GetMapping("/login")
+    public String getLoginPage(Model model) {
+        return "login";
+    }
+}
