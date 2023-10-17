bf923c8303f42dd508af733950c432a0401bf114
StoneHee99 <kiit0901@gmail.com>
2023-10-17 10:41:45 +0900

    chore: add thymeleaf dependency and controller template that returns thymeleaf model (LoginController)

---
+++ ministory/src/main/java/com/example/ministory/controller/UserApiController.java
@@ -0,0 +1,39 @@
+package com.example.ministory.controller;
+
+import com.example.ministory.entity.User;
+import com.example.ministory.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Controller
+@RequestMapping("/api/user")
+public class UserApiController {
+
+    private final UserRepository userRepository;
+
+    @GetMapping("/find-all")
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/signup")
+    public String getSignUp(UserEntityDto userEntityDto) {
+        return "userCreateForm";
+    }
+
+    @PostMapping("/signup")
+    public String postSignUp(@Valid UserEntityDto userEntityDto, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "userCreateForm";
+        }
+        return "redirect:/";
+    }
+}
